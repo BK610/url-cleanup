a4c53a214c73cbdc68315f2e1f67e50f469e8103
Bailey Kane <bailey.orion.kane@gmail.com>
2019-07-05 14:34:15 -0400

    demo with contextMenu working.

--- background.js
+++ background.js
@@ -1,29 +1,35 @@
-chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
-    switch (request.method) {
-        case 'getClipboard':
-            sendResponse(getClipboard());
-            break;
-        case 'setClipboard':
-            sendResponse(setClipboard(request.value));
-            break;
-        default:
-            console.error('Unknown method "%s"', request.method);
-            break;
-    }
-});
+'use strict';
+
+console.log("Background page started");
+
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     switch (request.method) {
+//         case 'getClipboard':
+//             console.log("getClipboard event");
+//             sendResponse(getClipboard());
+//             break;
+//         case 'setClipboard':
+//             console.log("setClipboard event");
+//             sendResponse(setClipboard(request.value));
+//             break;
+//         default:
+//             console.error('Unknown method "%s"', request.method);
+//             break;
+//     }
+// });
 
 function getClipboard() {
-    var result = null;
-    var textarea = document.getElementById('ta');
+    let result = null;
+    let textarea = document.getElementById('ta');
     textarea.value = '';
     textarea.select();
 
     if (document.execCommand('paste')) {
         result = textarea.value;
+        console.log("Got clipboard content: " + result);
     } else {
         console.error('failed to get clipboard content');
     }
-
     textarea.value = '';
     return result;
 }
@@ -49,6 +55,56 @@ function setClipboard(value) {
     return result;
 }
 
+function sendPasteToContentScript(toBePasted) {
+    // We first need to find the active tab and window and then send the data
+    // along. This is based on:
+    // https://developer.chrome.com/extensions/messaging
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {type: 'paste', data: toBePasted}
+        );
+    });
+}
+
+function sendMessageToUser(msg) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {type: 'message', message: msg}
+        );
+    });
+}
+
+/*---------------------------------------------------*/
+
+/**
+ * The function that will handle our context menu clicks.
+ */
+function onClickHandler(info, tab) {
+    var clipboardContent = getClipboard();
+    console.log('clipboardContent: ' + clipboardContent);
+    if (info.menuItemId === 'pasteDemo') {
+        console.log('clicked paste demo');
+        sendPasteToContentScript(clipboardContent);
+    }
+}
+
+// Register the click handler for our context menu.
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up the single one item "paste"
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.contextMenus.create(
+        {
+            'title': 'Paste Demo',
+            'id': 'pasteDemo',
+            'contexts': ['editable']
+        });
+});
+
+/*---------------------------------------------------*/
+
 //TODO:
 // Add handling for URLs without "http" start
 // Add handling for problems within the URL
@@ -62,6 +118,14 @@ function isUrl(copiedValue) {
     }
 }
 
+//TODO:
+// Add logic to keep searches
 function cleanUrl(url) {
+    let cleanedUrl = url;
+    let idx = cleanedUrl.indexOf("?");
+    if (idx !== -1) {
+        cleanedUrl = cleanedUrl.slice(0, idx);
+    }
 
+    return cleanedUrl;
 }
--- contentscript.js
+++ contentscript.js
@@ -2,28 +2,17 @@
 
 var extensionId = 'kdjihipfjekomponmajpffoomkecnkle';
 
-document.getElementById('copyButton').addEventListener('click', function() {
-    var text = document.getElementById('para').textContent;
-
-    chrome.runtime.sendMessage(
-        extensionId,
-        { method: 'setClipboard', value: text },
-        function(response) {
-            console.log('extension setClipboard response', response);
-        }
-    );
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    readClipboard();
-
-    // Re-read the clipboard every time the page becomes visible.
-    document.addEventListener('visibilitychange', function() {
-        if (!document.hidden) {
-            readClipboard();
-        }
-    });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     readClipboard();
+//     console.log("domcontentloaded");
+//
+//     // Re-read the clipboard every time the page becomes visible.
+//     document.addEventListener('visibilitychange', function() {
+//         if (!document.hidden) {
+//             readClipboard();
+//         }
+//     });
+// });
 
 function readClipboard() {
     chrome.runtime.sendMessage(
@@ -35,3 +24,52 @@ function readClipboard() {
         }
     );
 }
+
+// function insertTextAtCursor(text) {
+//     // This will let us use the undo history. It doesn't seem to be working for
+//     // the contentEditable="true" divs gmail uses, though. Text inputs but
+//     // doesn't undo.
+//     var el = document.activeElement;
+//     var e = document.createEvent('TextEvent');
+//     e.initTextEvent('textInput', true, true, null, text);
+//     el.focus();
+//     el.dispatchEvent(e);
+// }
+
+// chrome.runtime.onMessage.addListener(function(request) {
+//     if (request.type === 'paste') {
+//         insertTextAtCursor(request.data);
+//     } else if (request.type === 'message') {
+//         // currently we will assume that all of the messages are errors.
+//         console.error(request.message);
+//     }
+// });
+
+/*-------------------------------------------------*/
+
+
+function insertTextAtCursor(text) {
+    var el = document.activeElement;
+    var val = el.value;
+    var endIndex;
+    var range;
+    var doc = el.ownerDocument;
+    if (typeof el.selectionStart === 'number' &&
+        typeof el.selectionEnd === 'number') {
+        endIndex = el.selectionEnd;
+        el.value = val.slice(0, endIndex) + text + val.slice(endIndex);
+        el.selectionStart = el.selectionEnd = endIndex + text.length;
+    } else if (doc.selection !== 'undefined' && doc.selection.createRange) {
+        el.focus();
+        range = doc.selection.createRange();
+        range.collapse(false);
+        range.text = text;
+        range.select();
+    }
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.data) {
+        insertTextAtCursor(request.data);
+    }
+});
