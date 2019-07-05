8530a72030b88e69b740de1d4e72f4c9ee1bdadc
Bailey Kane <bailey.orion.kane@gmail.com>
2019-07-05 10:52:46 -0400

    Added handlers

--- background.js
+++ background.js
@@ -11,3 +11,35 @@ chrome.runtime.onMessageExternal.addListener(function(request, sender, sendRespo
             break;
     }
 });
+
+function getClipboard() {
+    var result = null;
+    var textarea = document.getElementById('ta');
+    textarea.value = '';
+    textarea.select();
+
+    if (document.execCommand('paste')) {
+        result = textarea.value;
+    } else {
+        console.error('failed to get clipboard content');
+    }
+
+    textarea.value = '';
+    return result;
+};
+
+function setClipboard(value) {
+    var result = false;
+    var textarea = document.getElementById('ta');
+    textarea.value = value;
+    textarea.select();
+
+    if (document.execCommand('copy')) {
+        result = true;
+    } else {
+        console.error('failed to get clipboard content');
+    }
+
+    textarea.value = '';
+    return result;
+};
